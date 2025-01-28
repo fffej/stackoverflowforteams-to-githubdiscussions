a888d152f35d0cbb163398c233bda805100a337f
Jeff Foster <jeff.foster@acm.org>
2025-01-28 13:42:03 +0000

    add rate limiting

--- github_discussions_client.py
+++ github_discussions_client.py
@@ -1,8 +1,12 @@
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
+from ratelimit import limits, sleep_and_retry
 import os
 
 class GitHubDiscussionsClient:
+
+    CALLS_PER_HOUR = 5000
+
     def __init__(self, token):
         # Initialize the GraphQL client
         transport = RequestsHTTPTransport(
@@ -10,6 +14,13 @@ class GitHubDiscussionsClient:
             headers={'Authorization': f'Bearer {token}'}
         )
         self.client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    # TODO: Almost certainly need to capture rate limiting here and retry? (since I've guessed at the amount)
+    @sleep_and_retry
+    @limits(calls=CALLS_PER_HOUR, period=3600)
+    def _execute_query(self, query, variables=None):
+        return self.client.execute(query, variable_values=variables)    
+
 
     def create_discussion(self, repository_id, category_id, title, body):
         """Create a new discussion in the specified repository."""
@@ -33,7 +44,7 @@ class GitHubDiscussionsClient:
             }
         }
         
-        return self.client.execute(mutation, variable_values=variables)
+        return self._execute_query(mutation, variable_values=variables)
 
     def get_discussion_categories(self, owner, name):
         """Get available discussion categories for a repository."""
@@ -56,7 +67,7 @@ class GitHubDiscussionsClient:
             "name": name
         }
         
-        return self.client.execute(query, variable_values=variables)
+        return self._execute_query(query, variable_values=variables)
 
     def get_repository_id(self, owner, name):
         """Get the GitHub repository ID."""
@@ -73,7 +84,7 @@ class GitHubDiscussionsClient:
             "name": name
         }
         
-        return self.client.execute(query, variable_values=variables)
+        return self._execute_query(query, variable_values=variables)
 
     def create_comment(self, discussion_id, body):
         """Add a comment to an existing discussion."""
@@ -95,4 +106,4 @@ class GitHubDiscussionsClient:
             }
         }
         
-        return self.client.execute(mutation, variable_values=variables)
+        return self._execute_query(mutation, variable_values=variables)
